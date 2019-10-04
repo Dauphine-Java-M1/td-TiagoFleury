e068d3585a233cb43d8f7757b3c14540a428b75f
fleuti15 <fleuti15@b048-29.crio.dauphine.fr>
2019-10-04 16:14:03 +0200

    Initial import

---
+++ td00/src/main/java/fr/dauphine/ja/fleurytiago/td00/PrimeCollection.java
@@ -0,0 +1,52 @@
+package fr.dauphine.ja.fleurytiago.td00;
+import java.util.ArrayList;
+import java.lang.Math;
+
+/**
+ * Hello world!
+ *
+ */
+public class PrimeCollection {
+	private ArrayList<Integer> numbers;
+	
+	public PrimeCollection() {
+		numbers = new ArrayList<Integer>();
+	}
+	
+	
+	public void initRandom(int n, int m) {
+		java.util.Random randomGenerator = new java.util.Random();
+		for(int i=0;i<n;i++) {
+			numbers.add( (Integer)randomGenerator.nextInt(m) );
+		}
+	}
+	
+	private boolean isPrime(int p) {
+		if(p == 1)return false;
+		for(int i=1;i<Math.sqrt(p);i++) {
+			if(p%i == 0)return true;
+		}
+		return false;
+	}
+	
+	public void printPrimes() {
+		for(Integer n:numbers) {
+			if(isPrime(n))System.out.print(n+",");
+		}
+		System.out.println();
+	}
+	
+	public static void main(String[] args) {
+		PrimeCollection test = new PrimeCollection();
+		test.initRandom(100, 1000);
+		test.printPrimes();
+	}
+	
+	
+	public java.util.ArrayList<Integer> getNumbers() {
+		return numbers;
+	}
+	public void setNumbers(java.util.ArrayList<Integer> numbers) {
+		this.numbers = numbers;
+	}
+}
