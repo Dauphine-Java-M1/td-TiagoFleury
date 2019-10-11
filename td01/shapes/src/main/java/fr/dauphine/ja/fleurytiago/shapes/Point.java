5ec9de76f75c9fac8e87714198496d86bd5d84cf
fleuti15 <fleuti15@b032-03.crio.dauphine.fr>
2019-10-11 14:55:57 +0200

    import td01

---
+++ td01/shapes/src/main/java/fr/dauphine/ja/fleurytiago/shapes/Point.java
@@ -0,0 +1,95 @@
+package fr.dauphine.ja.fleurytiago.shapes;
+
+import java.util.ArrayList;
+
+public class Point {
+	
+	private Integer x;
+	private Integer y;
+	public static int numberOfPoints=0;
+	
+	///CONSTRUCTEURS
+	
+	public Point(int x, int y) {
+		numberOfPoints++;
+		this.x=x;
+		this.y=y;
+	}
+	
+	public Point() {
+		numberOfPoints++;
+		x=0;
+		y=0;
+	}
+	
+	public Point(Point p) {
+		numberOfPoints++;
+		x = p.getX().intValue();
+		y = p.getY().intValue();
+	}
+	
+	
+    public static void main( String[] args ){
+    	
+    	Point p1=new Point(1,2);
+    	Point p2=p1;
+    	Point p3=new Point(1,2);
+    	ArrayList<Point> list = new ArrayList<>();
+    	list.add(p1);
+    	System.out.println(list.indexOf(p2));
+    	System.out.println(list.indexOf(p3));
+    	
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    @Override
+    public String toString() {
+    	return "("+x.toString()+";"+y.toString()+")";
+    }
+    
+    @Override
+    public boolean equals(Object o) {
+    	if(o instanceof Point) {
+    		Point p = (Point) o;
+			if(p.getX().intValue() == x.intValue() && p.getY().intValue() == y.intValue() )return true;
+			
+    	}
+    	return false;
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    //ACCESSEURS
+    
+    public Integer getX() {
+		return x;
+	}
+    public Integer getY() {
+		return y;
+	}
+    
+    //MUTATEURS
+    
+    public void setX(Integer x) {
+		this.x = x;
+	}
+    public void setY(Integer y) {
+		this.y = y;
+	}
+}
