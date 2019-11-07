6ec02f32b9d439334b7e156079bd305b518f1a23
fleuti15 <fleuti15@b042-17.crio.dauphine.fr>
2019-11-07 12:56:27 +0100

    ajout testRing

--- td01/shapes/src/test/java/fr/dauphine/ja/fleurytiago/shapes/TestRing.java
+++ td01/shapes/src/test/java/fr/dauphine/ja/fleurytiago/shapes/TestRing.java
@@ -5,6 +5,13 @@ import junit.framework.TestCase;
 public class TestRing extends TestCase {
 	
 	public void testEquals() {
-		Ring r = new Ring();
+		Point p = new Point(2,2);
+		Ring r = new Ring(p.clone(),4,3);
+		
+		Ring r2 = new Ring(p.clone(),4,3);
+		
+		assertTrue(r.equals(r2));
 	}
+	
+	
 }
